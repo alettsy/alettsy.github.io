ee6cfb56f13f3bbfbe004494a09ec38ae234477e
hexbun <bettsalex1@gmail.com>
2023-05-21 21:33:54 +0100

    Add base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'hexedbun.github.io/',
+	base: '/hexedbun.github.io/',
 	plugins: [svelte()],
 });
