9ed9ee877bd2b25dbfb1317b5a927f9f11409b6e
alettsy <betts.alex@icloud.com>
2023-12-09 19:06:19 +0000

    Add certificates popup

---
+++ src/lib/Certificates.svelte
@@ -0,0 +1,118 @@
+<script>
+    export let certificates;
+</script>
+
+<div class="certificates">
+    <p>All certificates can be found on my <a href="https://www.linkedin.com/in/alex-betts/">LinkedIn</a>
+        profile.</p>
+    {#each certificates as certificate}
+        <div class="certificate">
+            <div class="image">
+                <img src={'/certificates/' + certificate.image} alt={certificate.name} class="image"/>
+            </div>
+            <div class="details">
+                <h2 class="name">{certificate.name}</h2>
+                <div class="company-date">
+                    <div class="company">{certificate.company}</div>
+                    <span class="dot">•</span>
+                    <div class="date">{certificate.date}</div>
+                </div>
+                <div class="type">{certificate.type}</div>
+            </div>
+        </div>
+    {/each}
+</div>
+
+<style>
+    a {
+        color: var(--accent-color);
+    }
+
+    img {
+        width: 350px;
+        height: 250px;
+    }
+
+    .certificates {
+        overflow-y: scroll;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        height: 100vh;
+    }
+
+    .certificates .certificate:last-child {
+        border-bottom: none;
+    }
+
+    .certificate {
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        justify-content: space-evenly;
+        padding: 15px 0 15px 0;
+        border-bottom: 2px var(--accent-color) solid;
+        width: 95%;
+    }
+
+    .details {
+        text-align: left;
+    }
+
+    .name {
+        color: var(--accent-color);
+        font-weight: bold;
+    }
+
+    .company-date {
+        display: flex;
+    }
+
+    .dot {
+        color: var(--accent-color);
+        padding: 0 3px 0 3px;
+    }
+
+    /* TABLET */
+    @media only screen and (min-width: 1200px) {
+        .certificates {
+            text-align: center;
+            display: flex;
+            align-items: center;
+            width: 100%;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .certificate {
+            display: flex;
+            flex-direction: row;
+            align-content: center;
+            justify-content: space-around;
+            align-items: center;
+            width: 100%;
+        }
+
+        .name {
+            padding-right: 10px;
+        }
+
+        .certificate:nth-child(even) .name {
+            padding-left: 10px;
+        }
+
+        .certificate:nth-child(even) .details {
+            text-align: right;
+        }
+
+        .certificate:nth-child(even) .company-date {
+            flex-direction: row-reverse;
+        }
+
+        .certificates .certificate:nth-child(odd) {
+            flex-direction: row-reverse;
+        }
+    }
+</style>
--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -1,8 +1,10 @@
 <script>
+    import Certificates from "./Certificates.svelte";
+
     export let data;
     export let show;
 
-    import { createEventDispatcher } from "svelte";
+    import {createEventDispatcher} from "svelte";
     import Control from "./Control.svelte";
     import WorkHistory from "./WorkHistory.svelte";
 
@@ -17,11 +19,13 @@
 
 <div class="popup" class:show-popup={show}>
     <div class="close">
-        <Control text="Close" onclick={toggle} />
+        <Control text="Close" onclick={toggle}/>
     </div>
     <div class="content">
         {#if data.type == "work-history"}
-            <WorkHistory jobs={data.data.jobs} />
+            <WorkHistory jobs={data.data.jobs}/>
+        {:else if data.type === 'certificates'}
+            <Certificates certificates={data.data.certificates}/>
         {:else}
             <p>Unable to show popup</p>
         {/if}
